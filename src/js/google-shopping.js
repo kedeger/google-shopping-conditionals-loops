e2bd183d34093fd0590ad7bbdaec4f172a8c939a
Kemal Deger <kedeger@uw.edu>
2016-05-25 19:05:26 -0700

    Google shopping deliverable (Kemal)

--- src/js/google-shopping.js
+++ src/js/google-shopping.js
@@ -1,5 +1,56 @@
 var data = require('../products.json');
+var counter = 0
 
 // Write your solutions below
 
-console.log(data.items);
+//console.log(data.items.kind["shopping#products"]);
+
+/* Answer for #1
+for (var i = 0; i < data.items.length; i++) {
+  console.log(data.items[i].kind.includes("shopping#product"));
+if (data.items[i].kind.includes("shopping#product")) {
+     counter++;
+}
+console.log(counter);
+}
+*/
+
+/* Answer for #2
+for (var i = 0; i < data.items.length; i++) {
+if (data.items[i].product.inventories[0].availability=="backorder") {
+     console.log(data.items[i].product.title);
+}
+}
+*/
+
+/* Answer for #3
+for (var i = 0; i < data.items.length; i++) {
+if (data.items[i].product.images.length > 1) {
+     console.log(data.items[i].product.title);
+}
+}
+*/
+
+/* Answer for #4
+for (var i = 0; i < data.items.length; i++) {
+if (data.items[i].product.brand=="Canon") {
+     console.log(data.items[i].product.title);
+}
+}
+*/
+
+/* Answer for #5
+for (var i = 0; i < data.items.length; i++) {
+if (data.items[i].product.brand=="Canon")
+if (data.items[i].product.author.name=="eBay")
+console.log(data.items[i].product.title);
+}
+*/
+
+
+//Answer for #6!!!!!!!!!!!!!!!!!!!!  (:
+for (var i = 0; i < data.items.length; i++) {
+  console.log(data.items[i].product.brand)
+  console.log(data.items[i].product.inventories[0].price)
+  console.log(data.items[i].product.link)
+}
